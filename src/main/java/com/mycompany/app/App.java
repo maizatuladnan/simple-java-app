7a2d054d8b484403fd9bb23c3f4d804a0de998e8
maizatul akma <maizatuladnan98@gmail.com>
2023-05-31 08:22:37 +0000

    Test SCM polling

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Today is such a great day");
+        System.out.println("Training day");
     }
 
     public String getMessage() {
