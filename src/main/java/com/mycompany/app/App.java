3cd1a143d127c862c87941060af7bd18d301d154
maizatul akma <maizatuladnan98@gmail.com>
2023-05-31 07:31:57 +0000

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Today is such a hot day");
+        System.out.println("Today is such a great day");
     }
 
     public String getMessage() {
