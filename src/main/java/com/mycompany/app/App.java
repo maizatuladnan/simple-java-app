a58435debcc82b6fcb29ff129958cf89de623ba9
maizatul akma <maizatuladnan98@gmail.com>
2023-06-01 05:29:11 +0000

    Test pollSCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Last change for today");
+        System.out.println("Test pollSCM");
     }
 
     public String getMessage() {
